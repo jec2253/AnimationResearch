49df59d0794de5f0a4b497699cb2555a500b78ba
jec2253 <43836874+jec2253@users.noreply.github.com>
2018-12-05 12:34:48 -0500

    New Website 12/5

---
+++ data.js
@@ -0,0 +1,485 @@
+ 
+      var data = {
+        "id1": {
+          num: "Image 1 :   ",
+          id: "McDonald's goodnight",
+          image_src: "mcD-GN.gif",
+          cap: "primary: misc <br> secondary: none",
+          categories: ["all", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: []
+        },
+        "id2": {
+          num: "Image 2 :   ",
+          id: "Nike",
+          image_src: "Nike_justDoIt.gif",
+          cap: "primary: spin <br> secondary: none",
+          categories: ["all", "spin"],
+          primary_category: ["spin"],
+          secondary_categories: []
+        },
+        "id3": {
+          num: "Image 3 :   ",
+          id: "Cone",
+          image_src: "Cone_earth1.gif",
+          cap: "primary: linear <br> secondary: misc",
+          categories: ["all", "linear", "misc"],
+          primary_category: ["linear"],
+          secondary_categories: ["misc"]
+        },
+        "id4": {
+          num: "Image 4 :   ",
+          id: "Cloud",
+          image_src: "Cloud.gif",
+          cap: "primary: linear <br> secondary: misc",
+          categories: ["miscs", "linear", "all"],
+          primary_category: ["linear"],
+          secondary_categories: ["misc"]
+        },
+        "id5": {
+          num: "Image 5 :   ",
+          id: "Mount-Sinai",
+          image_src: "Mount-Sinai.gif",
+          cap: "primary: linear <br> secondary: misc",
+          categories: ["misc", "all", "linear"],
+          primary_category: ["linear"],
+          secondary_categories: ["misc"]
+        },
+        "id6": {
+          num: "Image 6 :   ",
+          id: "Nivea",
+          image_src: "Nivea-NightCream.gif",
+          cap: "primary: linear <br> secondary: none",
+          categories: ["all", "linear"],
+          primary_category: ["linear"],
+          secondary_categories: []
+        },
+        "id7": {
+          num: "Image 7 :   ",
+          id: "PowerUpCoffee",
+          image_src: "PowerUpCoffee.gif",
+          cap: "primary: misc <br> secondary: none",
+          categories: ["all", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: []
+        },
+        "id8": {
+          num: "Image 8 :   ",
+          id: "WOW_SOUP2",
+          image_src: "WOW_SOUP2.gif",
+          cap: "primary: spin <br> secondary: misc",
+          categories: ["all", "misc", "spin"],
+          primary_category: ["spin"],
+          secondary_categories: ["misc"]
+        },
+        "id9": {
+          num: "Image 9 :   ",
+          id: "McDFriWifi",
+          image_src: "McDFriWifi.gif",
+          cap: "primary: misc <br> secondary: none",
+          categories: ["all", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: []
+        },
+        "id10": {
+          num: "Image 10 :   ",
+          id: "Help_handreach",
+          image_src: "Help_handreach.gif",
+          cap: "primary: linear <br> secondary: misc",
+          categories: ["all", "misc", "linear"],
+          primary_category: ["linear"],
+          secondary_categories: ["misc"]
+        },
+        "id11": {
+          num: "Image 11 :   ",
+          id: "PeaceNotWar",
+          image_src: "PeaceNotWar.gif",
+          cap: "primary: linear <br> secondary: misc",
+          categories: ["all", "misc", "linear"],
+          primary_category: ["linear"],
+          secondary_categories: ["misc"]
+        },
+        "id12": {
+          num: "Image 12 :   ",
+          id: "The_Loop",
+          image_src: "The_Loop.gif",
+          cap: "primary: linear <br> secondary: none",
+          categories: ["all", "linear"],
+          primary_category: ["linear"],
+          secondary_categories: []
+        },
+        "id13": {
+          num: "Image 13 :   ",
+          id: "blood-lungs",
+          image_src: "blood-lungs.gif",
+          cap: "primary: misc <br> secondary: none",
+          categories: ["all", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: []
+        },
+        "id14": {
+          num: "Image 14 :   ",
+          id: "Nike_lung",
+          image_src: "Nike_lung.gif",
+          cap: "primary: expand/compress <br> secondary: none",
+          categories: ["all", "expandcompress"],
+          primary_category: ["expandcompress"],
+          secondary_categories: []
+        },
+        "id15": {
+          num: "Image 15 :   ",
+          id: "Nike_run",
+          image_src: "nike_run.gif",
+          cap: "primary: linear <br> secondary: sequential",
+          categories: ["all", "linear", "sequential"],
+          primary_category: ["linear"],
+          secondary_categories: ["sequential"]
+        },
+        "id16": {
+          num: "Image 16 :   ",
+          id: "Alarm-McSandwich",
+          image_src: "Alarm-McSandwich.gif",
+          cap: "primary: linear <br> secondary: spin",
+          categories: ["all", "spin", "linear"],
+          primary_category: ["linear"],
+          secondary_categories: ["spin"]
+        },
+        "id17": {
+          num: "Image 17 :   ",
+          id: "US_ARMY_SAM",
+          image_src: "US_ARMY_SAM.gif",
+          cap: "primary: misc <br> secondary: none",
+          categories: ["all", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: []
+        },
+        "id18": {
+          num: "Image 18 :   ",
+          id: "Christmas_Starbucks",
+          image_src: "Christmas_Starbucks.gif",
+          cap: "primary: misc <br> secondary: none",
+          categories: ["all", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: []
+        },
+        "id19": {
+          num: "Image 19 :   ",
+          id: "Divorce",
+          image_src: "Divorce.gif",
+          cap: "primary: linear <br> secondary: none",
+          categories: ["all", "linear"],
+          primary_category: ["linear"],
+          secondary_categories: []
+        },
+        "id20": {
+          num: "Image 20 :   ",
+          id: "Money_Idea",
+          image_src: "Money_Idea.gif",
+          cap: "primary: sequential <br> secondary: linear",
+          categories: ["all", "linear", "sequential"],
+          primary_category: ["sequential"],
+          secondary_categories: ["linear"]
+        },
+        "id21": {
+          num: "Image 21 :   ",
+          id: "Wine",
+          image_src: "Wine.gif",
+          cap: "primary: sequential <br> secondary: linear",
+          categories: ["all", "linear", "sequential"],
+          primary_category: ["sequential"],
+          secondary_categories: ["linear"]
+        },
+        "id22": {
+          num: "Image 22 :   ",
+          id: "Gun_Car",
+          image_src: "Gun_Car.gif",
+          cap: "primary: misc <br> secondary: none",
+          categories: ["all", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: []
+        },
+        "id23": {
+          num: "Image 23 :   ",
+          id: "Creative_minds",
+          image_src: "Creative_minds.gif",
+          cap: "primary: misc <br> secondary: none",
+          categories: ["all", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: []
+        },
+        "id24": {
+          num: "Image 24 :   ",
+          id: "world_yours",
+          image_src: "world_yours.gif",
+          cap: "primary: spin <br> secondary: misc",
+          categories: ["all", "misc", "spin"],
+          primary_category: ["spin"],
+          secondary_categories: ["misc"]
+        },
+        "id25": {
+          num: "Image 25 :   ",
+          id: "knitting",
+          image_src: "knitting.gif",
+          cap: "primary: spin <br> secondary: misc",
+          categories: ["all", "spin", "misc"],
+          primary_category: ["spin"],
+          secondary_categories: ["misc"]
+        },
+        "id26": {
+          num: "Image 26 :   ",
+          id: "i_accept",
+          image_src: "i_accept.gif",
+          cap: "primary: sequential <br> secondary: none",
+          categories: ["all", "sequential"],
+          primary_category: ["sequential"],
+          secondary_categories: []
+        },
+        "id27": {
+          num: "Image 27 :   ",
+          id: "beaker",
+          image_src: "beaker.gif",
+          cap: "primary: misc <br> secondary: none",
+          categories: ["all", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: []
+        },
+        "id28": {
+          num: "Image 28 :   ",
+          id: "trump_tweets",
+          image_src: "trump_tweets.gif",
+          cap: "primary: linear <br> secondary: none",
+          categories: ["all", "linear"],
+          primary_category: ["linear"],
+          secondary_categories: []
+        },
+        "id29": {
+          num: "Image 29 :   ",
+          id: "bigger_brains",
+          image_src: "bigger_brains.gif",
+          cap: "primary: misc <br> secondary: none",
+          categories: ["all", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: []
+        },
+        "id30": {
+          num: "Image 30 :   ",
+          id: "looking_love",
+          image_src: "looking_love.gif",
+          cap: "primary: linear <br> secondary: sequential, misc",
+          categories: ["all", "linear", "sequential", "misc"],
+          primary_category: ["linear"],
+          secondary_categories: ["sequential", "misc"]
+        },
+        "id31": {
+          num: "Image 31 :   ",
+          id: "covering_past",
+          image_src: "covering_past.gif",
+          cap: "primary: sequential <br> secondary: misc",
+          categories: ["all", "sequential", "misc"],
+          primary_category: ["sequential"],
+          secondary_categories: ["misc"]
+        },
+        "id32": {
+          num: "Image 31 :   ",
+          id: "pizza",
+          image_src: "pizza.gif",
+          cap: "primary: sequential <br> secondary: misc",
+          categories: ["all", "sequential", "misc"],
+          primary_category: ["sequential"],
+          secondary_categories: ["misc"]
+        },
+        "id33": {
+          num: "Image 33 :   ",
+          id: "Chinas_econstats_cred",
+          image_src: "Chinas_econstats_cred.gif",
+          cap: "primary: misc <br> secondary: linear",
+          categories: ["all", "linear", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: ["linear"]
+        },
+        "id53": {
+          num: "Image 34 :   ",
+          id: "brexit_slow",
+          image_src: "brexit_slow.gif",
+          cap: "primary: linear <br> secondary: none",
+          categories: ["all", "linear"],
+          primary_category: ["linear"],
+          secondary_categories: []
+        },
+        "id34": {
+          num: "Image 35 :   ",
+          id: "doubt",
+          image_src: "doubt.gif",
+          cap: "primary: misc <br> secondary: linear",
+          categories: ["all", "linear", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: ["linear"],
+        },
+        "id35": {
+          num: "Image 36 :   ",
+          id: "watermelon",
+          image_src: "watermelon.gif",
+          cap: "primary: misc <br> secondary: sequential",
+          categories: ["all", "sequential", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: ["sequential"]
+        },
+        "id36": {
+          num: "Image 37 :   ",
+          id: "popsicle",
+          image_src: "popsicle.gif",
+          cap: "primary: misc <br> secondary: none",
+          categories: ["all", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: []
+        },
+        "id37": {
+          num: "Image 38 :   ",
+          id: "mixtape",
+          image_src: "mixtape.gif",
+          cap: "primary: spin <br> secondary: none",
+          categories: ["all", "spin"],
+          primary_category: ["spin"],
+          secondary_categories: []
+        },
+        "id38": {
+          num: "Image 39 :   ",
+          id: "inflating",
+          image_src: "inflating.gif",
+          cap: "primary: misc <br> secondary: none",
+          categories: ["all", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: []
+        },
+        "id39": {
+          num: "Image 40 :   ",
+          id: "big_ideas",
+          image_src: "big_ideas.gif",
+          cap: "primary: misc <br> secondary: none",
+          categories: ["all", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: []
+        },
+        "id40": {
+          num: "Image 41 :   ",
+          id: "merrell",
+          image_src: "merrell.gif",
+          cap: "primary: misc <br> secondary: sequential",
+          categories: ["miscp", "all", "sequential"],
+          primary_category: ["misc"],
+          secondary_categories: ["sequential"]
+        },
+        "id42": {
+          num: "Image 42 :   ",
+          id: "shoppingsocial",
+          image_src: "shoppingsocial.gif",
+          cap: "primary: linear <br> secondary: none",
+          categories: ["all", "linear"],
+          primary_category: ["linear"],
+          secondary_categories: []
+        },
+        "id43": {
+          num: "Image 43 :   ",
+          id: "pop-up-shop",
+          image_src: "pop-up-shop.gif",
+          cap: "primary: misc <br> secondary: none",
+          categories: ["all", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: []
+        },
+        "id44": {
+          num: "Image 44 :   ",
+          id: "porschegift",
+          image_src: "porschegift.gif",
+          cap: "primary: misc <br> secondary: sequential",
+          categories: ["all", "misc", "sequential"],
+          primary_category: ["misc"],
+          secondary_categories: ["sequential"]
+        },
+        "id45": {
+          num: "Image 45 :   ",
+          id: "smoking_lungs",
+          image_src: "smoking_lungs.gif",
+          cap: "primary: misc <br> secondary: sequential, linear",
+          categories: ["all", "misc", "sequential", "linear"],
+          primary_category: ["misc"],
+          secondary_categories: ["sequential", "linear"]
+        },
+        "id46": {
+          num: "Image 46 :   ",
+          id: "economist_1",
+          image_src: "economist_1.gif",
+          cap: "primary: misc <br> secondary: none",
+          categories: ["all", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: []
+        },
+        "id47": {
+          num: "Image 47 :   ",
+          id: "economist_2",
+          image_src: "economist_2.gif",
+          cap: "primary: linear <br> secondary: none",
+          categories: ["all", "linear"],
+          primary_category: ["linear"],
+          secondary_categories: []
+        },
+        "id48": {
+          num: "Image 48 :   ",
+          id: "nyt1",
+          image_src: "nyt1.gif",
+          cap: "primary: spin <br> secondary: sequential, misc",
+          categories: ["spinp", "misc", "sequential", "all"],
+          primary_category: ["spin"],
+          secondary_categories: ["sequential", "misc"]
+        },
+        "id49": {
+          num: "Image 49 :   ",
+          id: "nyt2",
+          image_src: "nyt2.gif",
+          cap: "primary: misc <br> secondary: none",
+          categories: ["all", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: []
+        },
+        "id50": {
+          num: "Image 50 :   ",
+          id: "nyt3",
+          image_src: "nyt3.gif",
+          cap: "primary: misc <br> secondary: linear",
+          categories: ["all", "linear", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: ["linear"]
+        },
+        "id51": {
+          num: "Image 51 :   ",
+          id: "nyt4",
+          image_src: "nyt4.gif",
+          cap: "primary: misc <br> secondary: none",
+          categories: ["all", "misc"],
+          primary_category: ["misc"],
+          secondary_categories: []
+        },
+        "id52": {
+          num: "Image 52 :   ",
+          id: "nyt5",
+          image_src: "nyt5.gif",
+          cap: "primary: misc <br> secondary: spin",
+          categories: ["all", "misc", "spin"],
+          primary_category: ["misc"],
+          secondary_categories: ["spin"]
+      },
+      }
+
+
+      var labels = {
+      	all: "All",
+      	misc: "Miscellaneous",
+      	spin: "Spin in place",
+      	linear: "Linear path",
+      	sequential: "Sequential addition of elements",
+      	expandcompress: "Expand, compress"
+      }
+
+
+
+    
---
+++ helpers.js
@@ -0,0 +1,210 @@
+      var count = 0; 
+
+      var active_type = "all";
+
+      $(document).ready(function() {
+      	loadDataPrim()
+        loadDataSec()
+      })
+/*
+        var container_div_1 = $("<div>")
+        $("container_div_1").html("")
+        container_div_1.attr('class',"container")
+        $("#parent").append(container_div_1)
+
+        var itemList = $("<div id='item_list'>")
+        var label = $("<h1>")
+        label.text(labels[active_type])
+        $("#parent").append(itemList)
+*/
+function loadDataPrim(){
+
+        var container_div_1 = $("<div>")
+        $("container_div_1").html("")
+        container_div_1.attr('class',"container")
+        $("#parent").append(container_div_1)
+
+        var itemList = $("<div id='item_list'>")
+        var label = $("<h1>")
+        label.text(labels[active_type])
+        $("#parent").append(itemList)
+
+
+        count = 0
+
+        var row = $("<div>")
+        row.attr('class', "row")
+        $("row").html("")
+        $(container_div_1).append(row)
+
+        numcol = 3; 
+        currcol = 1; 
+        currrow = 1; 
+
+        for(d in data){
+
+          var i = 0; 
+          var item = data[d]
+
+          var container_div = $("<div id='item_"+item['id']+"' class='container_div'>")
+
+          var caption = item.num + item.image_src
+          container_div.append("<br>" + caption.bold())
+          var catcap = item.cap 
+          container_div.append("<br>" + catcap + "<br>")
+          var image_div = $("<img style='width:60%'>").attr("src", item['image_src'])
+          container_div.append(image_div) 
+
+
+          for (c in item['primary_category']) {
+          	if (item['primary_category'][c] == active_type) {
+                  count++
+				          //$("#item_list").append(container_div)
+                  if (currcol <= numcol)
+                  {
+                    currcol++;
+                    col = $("<div>")
+                    col.attr("class", "col-md")
+                    $(row).append(col)  
+                    $(col).html(container_div)
+                  }
+                  else 
+                  {
+                    currcol = 2;
+                    row = $("<div>")
+                    row.attr('class', "row")
+                    $(container_div_1).append(row)
+                    col = $("<div>")
+                    col.attr("class", "col-md")
+                    $(row).append(col) 
+                    $(col).html(container_div)
+                  }
+          	}
+          }
+        }
+        
+        changeCount()
+
+      }  
+
+      var loadDataSec = function(){
+
+        var container_div_1 = $("<div>")
+        $("container_div_1").html("")
+        container_div_1.attr('class',"container")
+        $("#parent").append(container_div_1)
+
+        var itemList = $("<div id='item_list'>")
+        var label = $("<h1>")
+        label.text(labels[active_type])
+        $("#parent").append(itemList)
+
+        count = 0
+        $("#item_list").remove()
+        var row = $("<div>")
+        row.attr('class', "row")
+        $("row").html("")
+        $(container_div_1).append(row)
+
+        numcol = 3; 
+        currcol = 1; 
+        currrow = 1; 
+
+        for(d in data){
+
+          var i = 0; 
+          var item = data[d]
+
+          var container_div = $("<div id='item_"+item['id']+"' class='container_div'>")
+
+          var caption = item.num + item.image_src
+          container_div.append("<br>" + caption.bold())
+          var catcap = item.cap 
+          container_div.append("<br>" + catcap + "<br>")
+          var image_div = $("<img style='width:60%'>").attr("src", item['image_src'])
+          container_div.append(image_div) 
+
+          for (c in item['secondary_categories']) {
+            if (item['secondary_categories'][c] == active_type) {
+                  count++
+                  //$("#item_list").append(container_div)
+                  if (currcol <= numcol)
+                  {
+                    currcol++;
+                    col = $("<div>")
+                    col.attr("class", "col-md")
+                    $(row).append(col)  
+                    $(col).html(container_div)
+                  }
+                  else 
+                  {
+                    currcol = 2;
+                    row = $("<div>")
+                    row.attr('class', "row")
+                    $(container_div_1).append(row)
+                    col = $("<div>")
+                    col.attr("class", "col-md")
+                    $(row).append(col) 
+                    $(col).html(container_div)
+                 }
+            }
+          }
+
+        }
+        
+        changeCount()
+
+      }  
+
+      var setActivePrim = function(type) {
+        console.log("setActivePrim")
+      	active_type = type
+      	loadDataPrim() 
+      }    
+
+      var setActiveSec = function(type) {
+        console.log("setActiveSec")
+        active_type = type 
+        loadDataSec()
+      }
+
+     var changeHeader =  function (name) {
+         $("#myHeader").innerHTML = name;
+      }
+
+      var changeCount = function() {
+          $("#counter").innerHTML = "Number of animations:   " + count; 
+      }
+
+      /*
+             /* col1 = $("<div>")
+        col1.attr("class", "col-md")
+        $(new_row).append(col1)
+        //$(col1).html("col1")
+
+        col2 = $("<div>")
+        col2.attr("class", "col-md")
+        $(new_row).append(col2)
+        //$(col2).html("col222222")
+
+        col3 = $("<div>")
+        col3.attr("class", "col-md")
+        $(new_row).append(col3)
+        //$(col3).html("33333")
+
+        col4 = $("<div>")
+        col4.attr("class", "col-md")
+        $(new_row).append(col4)
+        //$(col4).html("444")
+
+
+         new row 
+        var new_row_2 = $("<div>")
+        new_row_2.attr('class', "row")
+        $(container_div).append(new_row_2)
+
+        col1_2 = $("<div>")
+        col1_2.attr("class", "col-md-4")
+        $(new_row_2).append(col1_2)
+        //$(col1_2).html("col1_2")
+*/
